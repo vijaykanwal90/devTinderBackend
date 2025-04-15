006e5e3278e5a8e5b31caaf1687bee0f139c0c5e
vijaykanwal <vijaykanwal90@gmail.com>
2025-04-15 18:42:48 +0530

    some changes

--- src/config/database.js
+++ src/config/database.js
@@ -5,9 +5,7 @@ const connectDB = async ()=>{
     const url = process.env.MONGO_URL;
     console.log(url)
     
-    await mongoose.connect(process.env.MONGO_URL
-      
-    )
+    await mongoose.connect(process.env.MONGO_URL)
 }
 
 module.exports = connectDB;
