2f0a9f2681bb7df9a5b3405c25c682b27ee8e7a6
vijaykanwal <vijaykanwal90@gmail.com>
2025-04-16 23:58:20 +0530

    local storage

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,8 +6,17 @@ const userAuth = async(req,res,next)=>{
     try {
         // const cookies = req.cookies;
         // console.log(cookies)
-        const {token} = req.cookies;
+        // const {token} = req.cookies;
         // console.log("token is " + token)
+        const authHeader = req.headers.authorization;
+
+        // Check if token exists in the Authorization header
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+          return res.status(401).json({ message: "No token provided" });
+        }
+      
+        // Extract token from header
+        const token = authHeader.split(" ")[1];
         if(!token){
         
            return  res.status(401).send("You are not logged in please login")
